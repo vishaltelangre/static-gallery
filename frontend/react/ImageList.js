f4f8e30d27a731b3153a0ef02ec90e89095592a3
Vishal Telangre <the@vishaltelangre.com>
2017-05-07 20:30:22 +0530

    Fix: Navigation links to prev/next photos are not respecting applied filtered on home page

--- frontend/react/ImageList.js
+++ frontend/react/ImageList.js
@@ -8,38 +8,11 @@ export default class ImageList extends React.Component {
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
   }
 
-  getItems() {
-    let items = []
-
-    if (this.props.items) {
-      const { filterType, filterValue } = this.props
-      items = this.props.items
-
-      if (filterType !== 'All' && filterValue !== 'All') {
-        if (filterType == 'Album') {
-          items = items.filter(image => {
-            return image.get('albums').findIndex(album => album == filterValue) > -1
-          })
-        } else if (filterType == 'Category') {
-          items = items.filter(image => {
-            return image.get('category') == filterValue
-          })
-        } else if (filterType == 'Tag') {
-          items = items.filter(image => {
-            return image.get('tags').findIndex(tag => tag == filterValue) > -1
-          })
-        }
-        return items
-      }
-    }
-    return items
-  }
-
   render() {
     return (<section>
       <ul className="imageList"
           style={{marginLeft: (document.body.clientWidth - (256+10)*4)*0.5}}>
-        {this.getItems().map(image =>
+        {this.props.filteredItems.map(image =>
           <ImageListItem key={image.get('slug')}
                          image={image} />
         )}
--- frontend/redux/reducer.js
+++ frontend/redux/reducer.js
@@ -1,4 +1,4 @@
-import {Map} from 'immutable'
+import {Map, List} from 'immutable'
 
  function setState(state, newState) {
    return state.merge(newState)
@@ -10,6 +10,32 @@ function changeFilterType(state, filterType) {
 
 function changeFilterValue(state, filterValue) {
   return state.set('filterValue', filterValue)
+}
+
+export function filteredItems(state) {
+  const filterType = state.get('filterType')
+  const filterValue = state.get('filterValue')
+  const items = state.get('items')
+
+  let _filteredItems = state.get('items') || List([])
+
+  if (items) {
+    if (filterType !== 'All' && filterValue !== 'All') {
+      if (filterType == 'Album') {
+        _filteredItems = items.filter(image => {
+          return image.get('albums').findIndex(album => album == filterValue) > -1
+        })
+      } else if (filterType == 'Category') {
+        _filteredItems = items.filter(image => image.get('category') == filterValue)
+      } else if (filterType == 'Tag') {
+        _filteredItems = items.filter(image => {
+          return image.get('tags').findIndex(tag => tag == filterValue) > -1
+        })
+      }
+    }
+  }
+
+  return _filteredItems
 }
 
 export default function (state = Map(), action) {
